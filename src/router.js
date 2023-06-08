6614bcc4d6d48bcc88a8dde847cc18ea4414487f
츄차원의세계 <lazpberry1012@gmail.com>
2023-06-08 23:48:49 +0900

    Set router

---
+++ src/router.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", );
+router.get("/wine/new");
+router.get("/wine/:id");
+router.get("/note/new");
+router.get("/note/:id");
